f5c009b92b18d31ceb4f20ff0c0c98b215480fcd
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2023-12-27 03:21:27 -0300

    Initializin System

--- accounts_nodejs/index.js
+++ accounts_nodejs/index.js
@@ -1,5 +1,22 @@
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const fs = require("fs");
 
-console.log('Started the Accounts');
+console.log("Started the Accounts");
+operation();
+
+function operation() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "O que você deseja fazer?",
+      choices: ["Criar Conta", "Consultar Saldo", "Depositar", "Sacar", "Sair"],
+    },
+  ]).then((answer) =>
+  {
+    const action = answer['action'];
+
+    console.log(action);
+  }).catch(err => console.log(err));
+}
